1750298b55bdbde9a553637dece65d862e2739fa
chauquocan <chauquocan2090@gmail.com>
2021-09-13 15:28:10 +0700

    commit master gom entity, repo, properties

--- src/main/java/com/quocan/bt_springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/quocan/bt_springdatajpa/repository/StudentRepository.java
@@ -4,7 +4,6 @@ import com.quocan.bt_springdatajpa.entity.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
 }
