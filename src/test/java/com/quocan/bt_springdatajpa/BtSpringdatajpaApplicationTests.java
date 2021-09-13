484d2fdbee5b20bacaa32d37e247bf0615493c81
chauquocan <chauquocan2090@gmail.com>
2021-09-13 15:55:20 +0700

    commit test repo_branch

--- src/test/java/com/quocan/bt_springdatajpa/BtSpringdatajpaApplicationTests.java
+++ src/test/java/com/quocan/bt_springdatajpa/BtSpringdatajpaApplicationTests.java
@@ -1,13 +1,25 @@
 package com.quocan.bt_springdatajpa;
 
+import com.quocan.bt_springdatajpa.entity.Student;
+import com.quocan.bt_springdatajpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BtSpringdatajpaApplicationTests {
+    @Autowired
+    private StudentRepository studentRepository;
 
+    //test sql cua repo_branch
     @Test
-    void contextLoads() {
+    private void insertStudent() {
+        Student student = Student.builder()
+                .firstName("quoc an")
+                .lastName("chau")
+                .email("guoan209")
+                .phone("1234")
+                .build();
+        studentRepository.save(student);
     }
-
 }
