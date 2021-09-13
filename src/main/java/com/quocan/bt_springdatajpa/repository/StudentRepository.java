3df1cd97a36cb637779ea5b854b6c83773f6361f
chauquocan <chauquocan2090@gmail.com>
2021-09-13 16:29:15 +0700

    commit test for jpql_branch

--- src/main/java/com/quocan/bt_springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/quocan/bt_springdatajpa/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.quocan.bt_springdatajpa.repository;
 
 import com.quocan.bt_springdatajpa.entity.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
+    @Query(value = "select s from Student s where s.lastName =?1")
+    public Student getStudentByLastName(String lastName);
 }
--- src/test/java/com/quocan/bt_springdatajpa/BtSpringdatajpaApplicationTests.java
+++ src/test/java/com/quocan/bt_springdatajpa/BtSpringdatajpaApplicationTests.java
@@ -1,13 +1,21 @@
 package com.quocan.bt_springdatajpa;
 
+import com.quocan.bt_springdatajpa.entity.Student;
+import com.quocan.bt_springdatajpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BtSpringdatajpaApplicationTests {
 
+    @Autowired
+    private StudentRepository repository;
+
+    //test jpql query for jpql_branch
     @Test
-    void contextLoads() {
+    void test() {
+        Student student = repository.getStudentByLastName("chau");
     }
 
 }
