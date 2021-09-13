4a3442195b3dc46cce6abfe98aa4467d9a4f14ed
chauquocan <chauquocan2090@gmail.com>
2021-09-13 16:17:38 +0700

    commit test for native_branch

--- src/main/java/com/quocan/bt_springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/quocan/bt_springdatajpa/repository/StudentRepository.java
@@ -1,9 +1,17 @@
 package com.quocan.bt_springdatajpa.repository;
 
 import com.quocan.bt_springdatajpa.entity.Student;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface StudentRepository extends CrudRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value = "select * from student", nativeQuery = true)
+    public List<Student> getStudents();
+
+
 }
+
--- src/test/java/com/quocan/bt_springdatajpa/BtSpringdatajpaApplicationTests.java
+++ src/test/java/com/quocan/bt_springdatajpa/BtSpringdatajpaApplicationTests.java
@@ -1,13 +1,22 @@
 package com.quocan.bt_springdatajpa;
 
+import com.quocan.bt_springdatajpa.entity.Student;
+import com.quocan.bt_springdatajpa.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class BtSpringdatajpaApplicationTests {
+    @Autowired
+    private StudentRepository repository;
 
+    //test native query for native_branch
     @Test
-    void contextLoads() {
+    void test() {
+        List<Student> students = repository.getStudents();
     }
 
 }
